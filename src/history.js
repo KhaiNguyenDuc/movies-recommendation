2c65ea9272f467b529a0eccef0cfc45e4dfbe842
KhaiNguyenDuc <duckhailinux@gmai.com>
2025-03-27 20:47:17 +0700

    Update

--- src/history.js
+++ src/history.js
@@ -2,4 +2,6 @@
 // We use normal router because of this and pass it our history object
 import { createBrowserHistory } from 'history';
 
-export default createBrowserHistory();
+export default createBrowserHistory({
+    basename: process.env.PUBLIC_URL, // This will take care of your GitHub Pages subdirectory
+});
